a1ef98f7dafa60f1031ed0e84e87179ee9e2478f
thomasprayon <thomasprayongowland@gmail.com>
2022-12-08 11:44:38 +0100

    Logic for stop counting after user or computer reaches 5pts

--- script.js
+++ script.js
@@ -1,66 +1,85 @@
 // Variables
-const rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
-const computerSelection = getComputerChoice();
-let playerSelection;
 let counterPlayer = 0;
 let counterComputer = 0;
-
-// Event listener for the images: rock, paper and scissors
-const imgs = document.querySelectorAll('#playerImg');
-
-imgs.forEach((img) => {
-    img.addEventListener('click', () => {
-        console.log('Img:', img);
-        console.log('Event happening!');
-    });
-});
+let count = 0;
+let playerScore = document.querySelector('#playerScore');
+let computerScore = document.querySelector('#computerScore');
 
 // Computer choice by making a random number between 0 a 1. An accessing through bracket notation.
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    return rockPaperScissors[randomNumber - 1];
-}
-function getPlayerChoice() {
-    playerSelection = prompt('Paper, Rock or Scissors?');
-    if (
-        playerSelection == null ||
-        playerSelection == undefined ||
-        playerSelection == ''
-    ) {
-        alert('Please choose Paper, Rock or Scissors!!');
-    } else {
-        console.log(
-            'Player correcting word:',
-            playerSelection[0].toUpperCase() +
-                playerSelection.slice(1).toLowerCase()
-        );
-        return (
-            playerSelection[0].toUpperCase() +
-            playerSelection.slice(1).toLowerCase()
-        );
-    }
+    const rockPaperScissors = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    const computerSelection = rockPaperScissors[randomNumber - 1];
+    return computerSelection;
 }
 
-// console.log('Computer choose:', getComputerChoice());
-// console.log('Player choose:', getPlayerChoice());
+function countRound() {
+    let roundPlaying = document.querySelector('#roundPlay');
+    count++;
+    roundPlaying.textContent = count;
+}
 
+// Add event listener => to every button and the selected weapon
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return console.log(
-            `DRAW: Computer choose ${computerSelection} and Player choose ${playerSelection}`
-        );
-    } else if (
-        (computerSelection == 'Rock' && playerSelection == 'Scissors') ||
-        (computerSelection == 'Paper' && playerSelection == 'Rock') ||
-        (computerSelection == 'Scissors' && playerSelection == 'Paper')
-    ) {
-        counterComputer++;
-        return console.log(`Computer WIN: ${counterComputer}`);
+    let computerChoice = document.querySelector('#computerChoose');
+    let imgComputer = document.querySelector('.start-computer');
+    const result = document.querySelector('#result');
+    const playingUntilFive = 5;
+    if (playingUntilFive === counterPlayer) {
+        result.textContent = 'Player WIN';
+    } else if (playingUntilFive === counterComputer) {
+        result.textContent = 'Computer WIN';
     } else {
-        playerSelection++;
-        return console.log(`Player WIN: ${counterPlayer}`);
+        if (playerSelection === computerSelection) {
+            computerChoice.textContent = computerSelection;
+            result.textContent = 'DRAW';
+            imgComputer.src = `/images/${computerSelection}.svg`;
+        } else if (
+            (playerSelection === 'rock' && computerSelection === 'scissors') ||
+            (playerSelection === 'paper' && computerSelection === 'rock') ||
+            (playerSelection === 'scissors' && computerSelection === 'paper')
+        ) {
+            computerChoice.textContent = computerSelection;
+            counterPlayer++;
+            result.textContent = 'Player win this round';
+            playerScore.textContent = counterPlayer;
+            imgComputer.src = `/images/${computerSelection}.svg`;
+        } else {
+            computerChoice.textContent = computerSelection;
+            counterComputer++;
+            result.textContent = 'Computer win this round';
+            computerScore.textContent = counterComputer;
+            imgComputer.src = `/images/${computerSelection}.svg`;
+        }
     }
 }
 
-// playRound(getPlayerChoice(), computerSelection);
-// console.log(`Computer: ${counterComputer} vs Player: ${counterPlayer}`);
+// Initial function where it shows the order in which every function should be called
+function playGame() {
+    let playerSelection;
+    const choosenByPlayer = document.querySelectorAll('button');
+    choosenByPlayer.forEach((weapon) => {
+        weapon.addEventListener('click', function () {
+            const whichWeapon = document.querySelectorAll('.player');
+            if (weapon.classList.contains('rock')) {
+                whichWeapon[0].classList.add('choosen');
+                whichWeapon[1].classList.remove('choosen');
+                whichWeapon[2].classList.remove('choosen');
+                playerSelection = 'rock';
+            } else if (weapon.classList.contains('paper')) {
+                whichWeapon[0].classList.remove('choosen');
+                whichWeapon[1].classList.add('choosen');
+                whichWeapon[2].classList.remove('choosen');
+                playerSelection = 'paper';
+            } else {
+                whichWeapon[0].classList.remove('choosen');
+                whichWeapon[1].classList.remove('choosen');
+                whichWeapon[2].classList.add('choosen');
+                playerSelection = 'scissors';
+            }
+            playRound(playerSelection, getComputerChoice());
+            countRound();
+        });
+    });
+}
+playGame();
